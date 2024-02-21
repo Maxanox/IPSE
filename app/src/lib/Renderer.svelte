e423f07019fb8d24e575fcb1dfd249f1cceb6ee9
Maxanox <Maxanox27@gmail.com>
2024-02-21 12:52:56 +0100

    feat: Adds a Renderer component and begins a demo of use

---
+++ app/src/lib/Renderer.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">  
+    import { onMount } from 'svelte';
+
+    export let width: number;
+    export let height: number;
+    export let class_: string;
+
+    let canvas: HTMLCanvasElement;
+    let ctx: CanvasRenderingContext2D;
+
+    onMount(() => {
+        ctx = canvas.getContext('2d')!;
+        if (ctx === null) {
+            throw new Error('2d context not supported');
+        }
+    });
+
+    export function clear() {
+        ctx.clearRect(0, 0, width, height);
+    }
+
+    export function drawRect(x: number, y: number, w: number, h: number, color: string = "white") {
+        ctx.fillStyle = color;
+        ctx.fillRect(x, y, w, h);
+    }
+
+    export function drawCircle(x: number, y: number, r: number, color: string = "white") {
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.arc(x, y, r, 0, Math.PI * 2);
+        ctx.fill();
+    }
+</script>
+
+<canvas bind:this={canvas} id="canvas" class="card {class_}" width="{width}" height="{height}"></canvas>
+
+<style></style>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Renderer from '$lib/Renderer.svelte';
+    import { onDestroy, onMount } from 'svelte';
+
+    let renderer: Renderer;
+
+    let rendererWidth: number = window.innerWidth*3/4;
+    let rendererHeight: number = window.innerHeight*3/4;
+  
+    onMount(() => {
+      window.addEventListener('resize', updateRendererDimensions);
+      test();
+    });
+  
+    onDestroy(() => {
+      window.removeEventListener('resize', updateRendererDimensions);
+    });
+
+    function updateRendererDimensions() {
+      rendererWidth = window.innerWidth*3/4;
+      rendererHeight = window.innerHeight*3/4;
+    }
+
+    function test() {
+      renderer.clear();
+      renderer.drawCircle(rendererWidth/2, rendererHeight/6, 40, "blue");
+    }
+</script>
+  
+<main class="container">
+    <Renderer 
+        bind:this={renderer} 
+        width={rendererWidth} 
+        height={rendererHeight}
+        class_="absolute inset-1/2 -translate-x-1/2 -translate-y-1/2"
+    />
+</main>
